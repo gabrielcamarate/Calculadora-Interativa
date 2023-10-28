94667a79b4d1cdf88108775c49a010a016eab9e1
daebakman <gabrielcamarate@icloud.com>
2023-10-28 08:29:41 -0300

    Criação do arquivo header, criação das funções: processKeypress, drawCalculatorButtons, drawCalculatorCase, initialize. Em resumo, mais uma etapa do refatoramento

---
+++ calculadora.h
@@ -0,0 +1,59 @@
+#pragma once
+
+// Caracteres que formam uma caixa (linha 10 até a linha 17)
+// Quinas
+#define TOP_RIGHT "\u2510"
+#define TOP_LEFT "\u250C"
+#define LOWER_LEFT "\u2514"
+#define LOWER_RIGHT "\u2518"
+
+// Prenchemento
+#define VERTICAL_LINE "\u2502"
+#define HORIZONTAL_LINE "\u2500"
+
+// Quantidade de colunas da calculadora (y)
+#define CALCULATOR_COLUMN 5
+
+// Quantidade de linhas da calculadora  (x)
+#define CALCULATOR_LINE 4
+
+// Tamanho do botão (minimo)
+#define HEIGHT_BUTTON 1
+
+// Tamanho da calculadora (minimo)
+#define HEIGHT_DEFAULT 22
+#define WIDITH_DEFAULT 34
+
+#define STR_CMP(x, y) (strcmp(x[i][j], y) == 0) // Macro de comparação
+#define MAX_CHARACTERS 100
+
+//
+#define BUTTON_ROWS 5
+#define BUTTON_COLUMNS 4
+
+// Assets
+void removeSpaces(char *originalString, int length);
+void showErrors(const char *message);
+char *allocateMemory();
+void initialize();
+
+// Criação das caixas
+void topRow(int width, int height, int startY, int startX);
+void middleRow(int width, int height, char *character, int startY, int startX);
+void lowerRow(int width, int height, int startY, int startX);
+void createDisplay(int width, int height, int startY, int startX);
+void createBox(int width, int height, char *character, int startY, int startX);
+
+// Criação da interface
+void menuWindow();
+void drawCalculatorCase(int yMax, int xMax);
+void drawCalculatorButtons(int *widthButton, int *startY, int *startX, int *line, int *column);
+
+// Operações matematicas
+void sum();
+void subtraction();
+void division();
+void multiplication();
+
+// Processamento
+void processKeypress(int keyPressed, int *line, int *column);
